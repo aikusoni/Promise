6ed0fd1952651dbec6bd0418caf79355b62fba6c
aikusoni <31752803+aikusoni@users.noreply.github.com>
2020-06-01 22:07:00 +0900

    Init

---
+++ Example.java
@@ -0,0 +1,80 @@
+public class Example {
+    public static void main(String[] args) {
+        test();
+    }
+
+    private static void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (Exception ex) {
+
+        }
+    }
+
+    private static void test() {
+        System.out.println("-begin-");
+        Promise p2 = Promise.onBegin(1, (p, i) -> {
+            // run
+            System.out.println("task#1 : " + (int)i);
+            p.resolve((int)i + 1);
+        })
+        .onThen((p, i) -> {
+            // run
+            System.out.println("task#2 : " + (int)i);
+            new Thread(() -> {
+                sleep(300);
+                p.resolve((int)i + 1);
+            }).start();
+        })
+        .onCatch((e) -> {
+            // not run
+            System.out.println("error : " + e.toString());
+        });
+
+        p2.onThen((p, i) -> {
+            // run
+            System.out.println("task#2-1 : " + (int)i);            
+            new Thread(() -> {
+                sleep(700);
+                p.resolve((int)i + 1);  
+            }).start();
+        })
+        .onThen((p, i) -> {
+            // run
+            System.out.println("task#2-1-1 : " + (int)i);
+            p.resolve((int)i + 1);  
+        })
+        .onCatch((e) -> {
+            // not run
+            System.out.println("error : " + e.toString());
+        });
+
+        p2.onThen((p, i) -> {
+            // run
+            System.out.println("task#2-2 : " + (int)i);
+            p.reject(new Exception("task#2-2 error!"));  
+        })
+        .onThen((p, i) -> {
+            // not run cause task#2-2 error
+            System.out.println("task#2-2-1 : " + (int)i);
+            p.reject(new Exception("task#2-2 error!"));  
+        })
+        .onCatch((e) -> {
+            // run
+            System.out.println("error : " + e.toString());
+        });
+
+        sleep(5000);
+        System.out.println("-end-");
+
+        // output
+        // -begin-
+        // task#1 : 1
+        // task#2 : 2
+        // task#2-1 : 3
+        // task#2-2 : 3
+        // error : java.lang.Exception: task#2-2 error!
+        // task#2-1-1 : 4
+        // -end-
+    }
+}
---
+++ Promise.java
@@ -0,0 +1,177 @@
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.HashSet;
+
+/**
+ * JAVA Promise
+ */
+public class Promise {
+    private ObservableTaskResult prevTaskResult;
+    private ConcurrentLinkedQueue<TaskDelegator> taskDelegators = new ConcurrentLinkedQueue<TaskDelegator>();
+    private ConcurrentLinkedQueue<ErrorTask> errorTasks = new ConcurrentLinkedQueue<ErrorTask>();
+
+    public interface Task {
+        void run(Publisher publisher, Object input);
+    }
+
+    public interface ErrorTask {
+        void run(Throwable errror);
+    }
+
+    public static Promise onBegin(Object input, Task task) {
+        TaskDelegator taskDelegator = new TaskDelegator(task);
+        Promise promise = new Promise(taskDelegator.getTaskResult());
+        taskDelegator.run(input);
+        return promise;
+    }
+
+    public Promise onThen(Task task) {
+        TaskDelegator taskDelegator = new TaskDelegator(task);
+        Promise promise = new Promise(taskDelegator.getTaskResult());
+        promise(taskDelegator);
+        return promise;
+    }
+
+    public Promise onCatch(ErrorTask errorTask) {
+        promise(errorTask);
+        return this;
+    }
+    
+    private Promise(ObservableTaskResult prevTaskResult) {
+        this.prevTaskResult = prevTaskResult;
+        this.prevTaskResult.attach(new Observer() {
+            @Override
+            public void onChanged() {
+                checkAndRunPromised();
+            }
+        });
+    }
+
+    private void promise(TaskDelegator taskDelegator) {
+        taskDelegators.offer(taskDelegator);
+        checkAndRunPromised();
+    }
+
+    private void promise(ErrorTask errorTask) {
+        errorTasks.offer(errorTask);
+        checkAndRunPromised();
+    }
+
+    private void checkAndRunPromised() {
+        if (this.prevTaskResult.getTaskState() == TaskState.COMPLETE) {
+            while (this.taskDelegators.peek() != null) {
+                TaskDelegator taskDelegator = this.taskDelegators.poll();
+                taskDelegator.run(this.prevTaskResult.getNextInput());
+            }
+        } else if (this.prevTaskResult.getTaskState() == TaskState.ERROR) {
+            while (this.errorTasks.peek() != null) {
+                ErrorTask errorTask = this.errorTasks.poll();
+                errorTask.run(this.prevTaskResult.getError());
+            }
+            while (this.taskDelegators.peek() != null) {
+                TaskDelegator taskDelegator = taskDelegators.poll();
+                taskDelegator.error(this.prevTaskResult.getError());
+            }
+        }
+    }
+
+    public static class Publisher {
+        private ObservableTaskResult taskResult;
+        Publisher(ObservableTaskResult taskResult) {
+            this.taskResult = taskResult;
+        }
+
+        void resolve(Object nextInput) {
+            taskResult.setCompleted(nextInput);
+        }
+
+        void reject(Throwable error) {
+            taskResult.setError(error);
+        }
+    }
+
+    private enum TaskState {
+        AWAIT,
+        COMPLETE,
+        ERROR
+    }
+
+    private static abstract class Observable {
+        private HashSet<Observer> observers = new HashSet<Observer>();
+        
+        protected void notifyChanged() {
+            for (Observer observer : observers) {
+                observer.onChanged();
+            }
+        }
+
+        void attach(Observer observer) {
+            observers.add(observer);
+        }
+
+        void detach(Observer observer) {
+            if (observers.contains(observer)) {
+                observers.remove(observer);
+            }
+        }
+    }
+
+    private interface Observer {
+        public void onChanged();
+    }
+
+    private static class ObservableTaskResult extends Observable {
+        private TaskState taskState = TaskState.AWAIT;
+        private Object nextInput = null;
+        private Throwable error = null;
+
+        synchronized void setCompleted(Object nextInput) {
+            if (taskState == TaskState.AWAIT) {
+                taskState = TaskState.COMPLETE;
+                this.nextInput = nextInput;
+                notifyChanged();
+            }
+        }
+
+        synchronized void setError(Throwable error) {
+            if (taskState == TaskState.AWAIT) {
+                taskState = TaskState.ERROR;
+                this.error = error;
+                notifyChanged();
+            }
+        }
+
+        synchronized TaskState getTaskState() {
+            return taskState;
+        }
+
+        synchronized Object getNextInput() {
+            return nextInput;
+        }
+
+        synchronized Throwable getError() {
+            return error;
+        }
+    } 
+
+    private static class TaskDelegator {
+        private Task task; 
+        private ObservableTaskResult taskResult;
+
+        TaskDelegator(Task task) {
+            this.task = task;
+            this.taskResult = new ObservableTaskResult();
+        }
+
+        void run(Object input) {
+            this.task.run(new Publisher(taskResult), input);
+        }
+
+        void error(Throwable error) {
+            this.taskResult.setError(error);
+        }
+        
+        ObservableTaskResult getTaskResult() {
+            return this.taskResult;
+        }
+    }
+}
